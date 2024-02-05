ca186bafe6c713630b630f73d20c9326ec586277
nalytovo <nalytovo@gmail.com>
2024-02-05 18:57:22 +0300

    database config

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 require('module-alias/register');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.listen(PORT, () => {
+  console.log(`The server is active on port ${PORT}`);
+});
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+	console.log('Database connection successful');
+	})
+	.catch((err) => {
+	console.error('Database connection failed');
+	});
+
